67f9350752f413ba7633e7954cdd956a6985d643
Martino <martinobennguyen@gmail.com>
2024-04-26 17:53:04 +1000

    Created General controller to side buttons, havent implemented code, reminder also isnt linked yet

---
+++ src/main/java/com/example/mindfulgamer/controller/GeneralController.java
@@ -0,0 +1,51 @@
+package com.example.mindfulgamer.controller;
+
+import javafx.fxml.FXML;
+
+public class GeneralController {
+    // This class is used to create the methods for clicking the buttons found in each window
+    // used to code redundancy
+
+    // Constructors
+
+
+    @FXML
+    public void Dashboard(){
+
+    }
+    @FXML
+    public void Gaming_Time(){
+
+    }
+    @FXML
+    public void Reminders(){
+
+    }
+
+    @FXML
+    public void Goals(){
+
+    }
+
+    @FXML
+    public void Achievements(){
+
+    }
+    @FXML
+    public void Healthy_Habits(){
+
+    }
+
+    @FXML
+    public void Logout(){
+
+    }
+    @FXML
+    public void Settings(){
+
+    }
+    @FXML
+    public void Profile(){
+
+    }
+}
