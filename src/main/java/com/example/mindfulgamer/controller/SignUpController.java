eda7dd091865e750883ef388ad9a44f908b2612f
Martino <martinobennguyen@gmail.com>
2024-05-24 19:14:12 +1000

    Signup Done

--- src/main/java/com/example/mindfulgamer/controller/SignUpController.java
+++ src/main/java/com/example/mindfulgamer/controller/SignUpController.java
@@ -13,7 +13,13 @@ import com.example.mindfulgamer.model.SqliteUserDAO;
 
 import java.io.IOException;
 
+/**
+ * Class used to sigh up for application, has validation, reading database etc.
+ */
 public class SignUpController {
+    /**
+     * Setting up ID for FXML and initialising variables
+     */
     private SqliteUserDAO userDAO;
     @FXML
     public Hyperlink login;
@@ -22,6 +28,9 @@ public class SignUpController {
     @FXML
     public TextField fname, lname, email, password;
 
+    /**
+     * Initialises the page such that it can detect what is written inside the text boxes
+     */
     @FXML
     public void initialize(){
         userDAO = new SqliteUserDAO();
@@ -32,6 +41,9 @@ public class SignUpController {
         password.textProperty().addListener((observable, oldValue, newValue) -> checkFields());
     }
 
+    /**
+     * Grabs all the strings in the textboxes when the sign up button is clicked
+     */
     public void checkFields(){
         String first_name = fname.getText();
         String last_name = lname.getText();
@@ -41,6 +53,11 @@ public class SignUpController {
         // Enable sign up button if all parameters are not null
         signup.setDisable(first_name.isEmpty() || last_name.isEmpty() || email_rec.isEmpty() || password_rec.isEmpty());
     }
+
+    /**
+     * Directs the user to the loging page only if certain sign up conditions are met
+     * @throws IOException throws the error in the terminal in case of unexpected errors
+     */
     @FXML
     protected void onSignUpButtonClick() throws IOException {
         String first_name = fname.getText();
@@ -72,8 +89,6 @@ public class SignUpController {
             showAlert("Email In Use", "This email is already being used.");
             return;
         }
-
-
         String hashedPassword = HashUtil.hash(password_rec);
         User newUser = new User(first_name_lower, last_name_lower, phone, email_lower, hashedPassword);
         userDAO.addUser(newUser);
@@ -88,22 +103,37 @@ public class SignUpController {
         // TESTING PURPOSES //
 
         // Redirect to the login page after signup
-
         Stage stage = (Stage) signup.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), HelloApplication.LOGIN_W, HelloApplication.LOGIN_H);
         stage.setScene(scene);
     }
+
+    /**
+     * Method used to check if password creation is valid
+     * @param password String, the input of the string for password
+     * @return bool, True if the string is allowed, false if otherwise
+     */
     public boolean isValidPassword(String password) {
         String regex = "^(?=.*[0-9])(?=.*[A-Z]).{8,}$"; //Contains 1 digit, one uppercase letter, and at least 8 characters long
         return password.matches(regex);
     }
 
+    /**
+     * Method used to check if email creation is valid
+     * @param email String, the input of the email
+     * @return bool, True if string is allowed, false otherwise
+     */
     public boolean isValidEmail(String email) {
         String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[a-zA-Z]{2,}$"; // Contains characters before @ symbol, the @ symbol, letters after @ symbol, "." and letters after ".".
         return email.matches(regex);
     }
 
+    /**
+     * Method used to create an Alert pop up
+     * @param title String, title of pop up
+     * @param message String, message of popup
+     */
     private void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
@@ -111,6 +141,12 @@ public class SignUpController {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+    /**
+     * Method used to create a success pop up
+     * @param title String, title of pop up
+     * @param message String, message of popup
+     */
     private void showSuccess(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
@@ -119,6 +155,10 @@ public class SignUpController {
         alert.showAndWait();
     }
 
+    /**
+     * Method used to direct user back to log in page when sign up hyperlink is clicked
+     * @throws IOException throws the error in the terminal in case of unexpected errors
+     */
     @FXML
     public void onLogInButtonClick() throws IOException{
         Stage stage = (Stage) signup.getScene().getWindow();
@@ -126,6 +166,4 @@ public class SignUpController {
         Scene scene = new Scene(fxmlLoader.load(), HelloApplication.LOGIN_W, HelloApplication.LOGIN_H);
         stage.setScene(scene);
     }
-
-
 }
