ec7800cfd92996411e93d37c79d27d86bd37ac80
Martino <martinobennguyen@gmail.com>
2024-05-24 19:21:04 +1000

    SqliteConn

--- src/main/java/com/example/mindfulgamer/model/SqliteConnection.java
+++ src/main/java/com/example/mindfulgamer/model/SqliteConnection.java
@@ -3,9 +3,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Class for creating a Sqlite database connection
+ */
 public class SqliteConnection {
+    /**
+     * Initialise instance connection
+     */
     private static Connection instance = null;
 
+    /**
+     * Creates a SQliteConnection
+     */
     private SqliteConnection() {
         String url = "jdbc:sqlite:users.db";
         try {
@@ -15,6 +24,10 @@ public class SqliteConnection {
         }
     }
 
+    /**
+     * Checks to see if the SQlite connection is running
+     * @return Connection instance of SQlite
+     */
     public static Connection getInstance() {
         if (instance == null) {
             new SqliteConnection();
